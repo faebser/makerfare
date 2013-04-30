0a959eaf73aa4e02644aa8665cb2581e34932cab
faebser <fabian.frei@esf-frei.ch>
2013-05-01 00:45:39 +0200

    added js module, new css styles

--- template/js/main.js
+++ template/js/main.js
@@ -1,1 +1,27 @@
+var ms = (function ($) {
+	// javascript module pattern
+	"use strict"; // enable strict mode for javascript module
+	// private vars
+	var module = {},
+	blocks = null,
+	classes = {
+		inactive : "inactive",
+		clickable : "clickable"
+	}
+	// private methods
+	// public methods
+	module.init = function () {
+		blocks = $(".block");
+		blocks.addClass(classes.inactive);
+		blocks.addClass(classes.clickable);
+		blocks.click(function () {
+			$(this).toggleClass(classes.inactive);
+		});
+	};
+	//return the module
+	return module;
+}($));
 
+$(document).ready(function() {
+	ms.init();
+});
