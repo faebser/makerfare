38c061b024597f2a8aebd1bd95f8ee5276085858
faebser <fabian.frei@esf-frei.ch>
2013-05-02 03:23:18 +0200

    major overhaul, added slider and reworked all headings

--- template/js/main.js
+++ template/js/main.js
@@ -3,20 +3,55 @@ var ms = (function ($) {
 	"use strict"; // enable strict mode for javascript module
 	// private vars
 	var module = {},
-	blocks = null,
+	blocks = $(".block").not(".simple"),
+	slider = $("#sliderOuter #slider"),
+	sliderElements = slider.find("li"),
+	sliderElementWidth = null,
+	sliderElementsAmount = null,
+	sliderElementsMaxMargin = null,
+	slideTime = 2000,
+	sliderId = null,
 	classes = {
 		inactive : "inactive",
-		clickable : "clickable"
-	}
+		clickable : "clickable",
+		sliderReady : "sliderReady"
+	};
 	// private methods
+	var clickHandlers = function () {
+		blocks.find("h2").click(function () {
+			$(this).parent().toggleClass(classes.inactive);
+		});
+		slider.hover(sliderPause, sliderContinue);
+	},
+	sliderNext = function () {
+		var currentMargin = parseInt(slider.css("margin-left")),
+		newMargin = null;
+		if(currentMargin == sliderElementsMaxMargin) {
+			newMargin = 0;
+		}
+		else {
+			newMargin = currentMargin + sliderElementWidth * -1;
+		}
+		slider.css("margin-left", newMargin);
+	},
+	sliderPause = function () {
+		window.clearInterval(sliderId);
+	},
+	sliderContinue = function () {
+		sliderId = window.setInterval(sliderNext, slideTime);
+	},
+	sliderMath = function () {
+		sliderElementsAmount = sliderElements.length;
+		sliderElementWidth = sliderElements.first().outerWidth();
+		sliderElementsMaxMargin = (sliderElementsAmount - 1) * sliderElementWidth * -1;
+	};
 	// public methods
 	module.init = function () {
-		blocks = $(".block");
-		blocks.addClass(classes.inactive);
-		blocks.addClass(classes.clickable);
-		blocks.click(function () {
-			$(this).toggleClass(classes.inactive);
-		});
+		blocks.addClass(classes.inactive + " " + classes.clickable);
+		sliderMath();
+		slider.parent().addClass(classes.sliderReady);
+		clickHandlers();
+		sliderContinue();
 	};
 	//return the module
 	return module;
