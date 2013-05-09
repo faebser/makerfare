5427895c6ac91eeca3f147a383d3dd23d9fdb59b
Fabian Frei <fabian.frei@esf-frei.ch>
2013-05-09 23:21:46 +0200

    fixed some stuff

--- template/js/main.js
+++ template/js/main.js
@@ -3,7 +3,11 @@ var ms = (function ($) {
 	"use strict"; // enable strict mode for javascript module
 	// private vars
 	var module = {},
+	win = $(window),
 	blocks = $(".block").not(".simple"),
+	articles = $("article"),
+	menu = $("#menuContainer"),
+	menuOffset = menu.offset().top,
 	slider = $("#sliderOuter #slider"),
 	sliderElements = slider.find("li"),
 	sliderElementWidth = null,
@@ -14,7 +18,10 @@ var ms = (function ($) {
 	classes = {
 		inactive : "inactive",
 		clickable : "clickable",
-		sliderReady : "sliderReady"
+		sliderReady : "sliderReady",
+		fixed : "fixed",
+		even : "even",
+		block : ".block"
 	};
 	// private methods
 	var clickHandlers = function () {
@@ -22,6 +29,7 @@ var ms = (function ($) {
 			$(this).parent().toggleClass(classes.inactive);
 		});
 		slider.hover(sliderPause, sliderContinue);
+		win.scroll(windowScrollTest);
 	},
 	sliderNext = function () {
 		var currentMargin = parseInt(slider.css("margin-left")),
@@ -44,6 +52,26 @@ var ms = (function ($) {
 		sliderElementsAmount = sliderElements.length;
 		sliderElementWidth = sliderElements.first().outerWidth();
 		sliderElementsMaxMargin = (sliderElementsAmount - 1) * sliderElementWidth * -1;
+	},
+	windowScrollTest = function () {
+		if(win.scrollTop() > menuOffset && !menu.hasClass(classes.fixed)) {
+			menu.addClass(classes.fixed);
+		}
+		else if(win.scrollTop() <= menuOffset && menu.hasClass(classes.fixed)) {
+			menu.removeClass(classes.fixed);
+		}
+	},
+	blockCounter = function () {
+		articles.each( function(index, element) {
+			console.log("article: " + $(element).attr("id"));
+			$(element).find(classes.block).each( function(index, element2) {
+				var e = $(element2);
+				console.log("index: " + e.index(classes.block));
+				if(e.index() % 2 == 0) {
+					e.addClass(classes.even);
+				}
+			});
+		});
 	};
 	// public methods
 	module.init = function () {
@@ -52,6 +80,7 @@ var ms = (function ($) {
 		slider.parent().addClass(classes.sliderReady);
 		clickHandlers();
 		sliderContinue();
+		blockCounter();
 	};
 	//return the module
 	return module;
